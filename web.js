5fd15faa8605d38ce2d55e91dc70aae4d4f82d58
Ron Yeh <ronyeh@gmail.com>
2013-07-06 16:14:38 -0700

    Load/Display index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    var responseString = fs.readFileSync("index.html", {encoding:"utf-8"});
+    response.send(responseString);
 });
 
 var port = process.env.PORT || 5000;
