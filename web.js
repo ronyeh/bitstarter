da3f9370e5739e4d56d4ffead621e240452117ff
Ron Yeh <ronyeh@gmail.com>
2013-07-19 03:22:27 -0700

    Added the actions panel, with links to our Twitter and Facebook pages.

--- web.js
+++ web.js
@@ -9,6 +9,15 @@ app.get('/', function(request, response) {
     response.send(responseString);
 });
 
+// app.get('/s/css/social-buttons.css', function(request, response) {
+//     var responseBuffer = fs.readFileSync("css/social-buttons.css");
+//     var responseString = new Buffer(responseBuffer).toString();
+//     response.send(responseString);
+// });
+
+app.use('/s', express.static(__dirname + "/s"));
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
